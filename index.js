8b12d8c7df3d56bb5bb7b45c52ed8765f91e9931
seenu433 <seenu433@gmail.com>
2022-05-05 19:45:16 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+ var proxy = httpProxy.createProxyServer({});// Creating Custom
+ 
+proxy.on('proxyReq', function(proxyReq, req, res, options) {
+  proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+});
+ 
+var server = http.createServer(function(req, res) {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
+  proxy.web(req, res, {
+    target: 'http://httpbin.org/get?answer=42',
+secure: true,
+changeOrigin: true
+  });
+});
+ 
+console.log("Proxy listening on port 80 ")
+server.listen(8080);
