857162726b3b9e30effaa9850ebd2d87279e97f5
seenu433 <seenu433@gmail.com>
2022-05-05 23:33:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
- var proxy = httpProxy.createProxyServer({});// Creating Custom
- 
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  proxyReq.setHeader('Content-Type', 'application/x-www-form-urlencoded');
+const express = require('express');
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// Create Express Server
+const app = express();
+
+// Configuration
+const PORT = 8010;
+const HOST = "localhost";
+const API_SERVICE_URL = "http://172.16.1.4:8010";
+
+// Logging
+app.use(morgan('dev'));
+
+// Info GET endpoint
+app.get('/info', (req, res, next) => {
+    res.send('This is a proxy service which proxies to JSONPlaceholder API.');
 });
- 
-var server = http.createServer(function(req, res) {
-  // You can define here your custom logic to handle the request
-  // and then proxy the request.
-  proxy.web(req, res, {
-    target: 'http://172.16.1.4:8010/v2/check',
-secure: false,
-changeOrigin: true
-  });
+
+// Authorization
+app.use('', (req, res, next) => {
+    if (req.headers.authorization) {
+        next();
+    } else {
+        res.sendStatus(403);
+    }
 });
- 
-console.log("Proxy listening on port 80 ")
-server.listen(8080);
+
+// Proxy endpoints
+app.use('/lang', createProxyMiddleware({
+    target: API_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/lang`]: '',
+    },
+}));
+
+// Start Proxy
+app.listen(PORT, HOST, () => {
+    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
